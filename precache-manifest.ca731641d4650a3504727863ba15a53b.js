ade04d5d4051e23c956612f51face2b6c6cfb150
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-01-02 10:56:24 +0200

    Updates

---
+++ precache-manifest.ca731641d4650a3504727863ba15a53b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464b90e05e7c87f0e5bab340ad3b7876",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "4f79cb124f542d132e75",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.0bb103b7.chunk.css"
+  },
+  {
+    "revision": "46a8929ab9b909a19fb1",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d0e490b3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d0e490b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f79cb124f542d132e75",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.72d5d692.chunk.js"
+  },
+  {
+    "revision": "86e8f61da2e8edff8d43",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.5b470970.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-phonebook/precache-manifest.d7877a5df95b8bd73d9f2008e87ad40f.js"
+  "/goit-react-hw-02-phonebook/precache-manifest.ca731641d4650a3504727863ba15a53b.js"
 );
 
 self.addEventListener('message', (event) => {
